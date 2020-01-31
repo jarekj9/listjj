9b89c40d74f5a94df60d910a14bf8e196bcc456c
jarekj9 <jerry.esp.sha@gmail.com>
2020-01-31 15:34:04 +0100

    test

--- hello/jarek.py
+++ hello/jarek.py
@@ -6,25 +6,23 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 def main():
     
-    try: 
-        connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-        cursor = connection.cursor() 
-        cursor.execute("CREATE TABLE vendors \
-              (vendor_id SERIAL PRIMARY KEY, vendor_name VARCHAR(255) NOT NULL);")
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor() 
+    cursor.execute("CREATE TABLE vendors (vendor_id SERIAL PRIMARY KEY, vendor_name VARCHAR(255) NOT NULL);")
+    conn.commit()
+    cursor.execute('INSERT INTO vendors(vendor_name) VALUES("Jarek");')
+    conn.commit()
+    cursor.execute("SELECT * FROM vendors;")
+    record = cursor.fetchone() 
+
+    #closing database conn. 
+    if(conn): 
+        cursor.close() 
+        conn.close() 
+        print("PostgreSQL conn is closed") 
         
-        cursor.execute('INSERT INTO vendors(vendor_name) \
-                        VALUES("Jarek");') 
-        cursor.execute("SELECT * FROM vendors;")
-        record = cursor.fetchone() 
-        return str(record)
-    except (Exception, psycopg2.Error) as error : 
-        print ("Error while connecting to PostgreSQL", error) 
-    finally: 
-        #closing database connection. 
-            if(connection): 
-                cursor.close() 
-                connection.close() 
-                print("PostgreSQL connection is closed") 
+    return str(record)
 if __name__ == '__main__':
     
     main()
