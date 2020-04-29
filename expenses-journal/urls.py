8af2a396807930993157810d100a2d6aa79c1a7c
jarekj9 <jerry.esp.sha@gmail.com>
2020-04-29 14:17:33 +0200

    test

---
+++ expenses-journal/urls.py
@@ -0,0 +1,37 @@
+from django.urls import path, include
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+import app.views
+
+#for auth:
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from django.conf.urls import include
+
+
+
+
+
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+urlpatterns = [
+    path("", app.views.index, name="index"),
+    path("db/", app.views.db, name="db"),
+    path("admin/", admin.site.urls),
+
+	path("register/", app.views.register, name="register"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
+]
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
