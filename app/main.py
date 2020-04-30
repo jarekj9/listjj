cad7d4f383097778eb615208a91b21462d9fe695
jarekj9 <jerry.esp.sha@gmail.com>
2020-04-30 15:05:42 +0200

    test

--- app/main.py
+++ app/main.py
@@ -4,26 +4,22 @@ from .models import *
 
 def main():
     # Create a new record using the model's constructor.
-    
+    output=""
     if not Categories.objects.filter(category='inne').count():
         record = Categories(category='inne')
         record.save()
     
     all_records = Categories.objects.all()
-    print('categories:')
-    for line in all_records:
-        print(line)
+    output.append(all_records)
     
     record = Journal(login='Jarek', value=100, category=Categories.objects.get(category='inne'), description='Test')
     record.save()
     
     
     all_records = Journal.objects.all()
-    print('all records:')
-    print(all_records)
-    print('end')
+    output.append(all_records)
     
-    return 'Jarek'
+    return output
     
 
 if __name__ == '__main__':
