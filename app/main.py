46fabd08d4fc67ba734e6b727b206572be5dcc73
jarekj9 <jerry.esp.sha@gmail.com>
2020-04-30 14:41:26 +0200

    test

--- app/main.py
+++ app/main.py
@@ -10,18 +10,18 @@ def main():
         record.save()
     
     all_records = Categories.objects.all()
+    print('categories:')
     for line in all_records:
         print(line)
     
     record = Journal(login='Jarek', value=100, category=Categories.objects.get(category='inne'), description='Test')
-
-    # Save the object into the database.
     record.save()
     
     
     all_records = Journal.objects.all()
-    
+    print('all records:')
     print(all_records)
+    print('end')
     
     
 
