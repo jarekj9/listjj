6d9b391fa0bcb269c043182472e608b94ac10d4f
jarekj9 <jerry.esp.sha@gmail.com>
2020-04-30 08:41:55 +0200

    test

--- app/models.py
+++ app/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+
+class Categories(models.Model):
+    category = models.CharField(max_length=100)
     
 class Journal(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
@@ -11,5 +15,4 @@ class Journal(models.Model):
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     description = models.CharField(max_length=1000)
     
-class Categories(models.Model):
-    category = models.CharField(max_length=100)
+
