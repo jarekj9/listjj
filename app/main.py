12ec64b655fff686165f4278bbb4bdb61e92be33
jarekj9 <jerry.esp.sha@gmail.com>
2020-05-04 12:38:15 +0200

    test

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ def main():
         output.append({'login':item.login,
                        'date':item.date,
                        'value':item.value,
-                       'category':Categories.objects.get(id=item.category.id).category,
+                       'category':item.category.category, #Categories.objects.get(id=item.category.id).category,
                        'description':item.description})
     
     return output
