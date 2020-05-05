ea75d611bd349990f5bd28299f88189aedc24882
jarekj9 <jerry.esp.sha@gmail.com>
2020-05-05 15:55:51 +0200

    test

--- expenses-journal/urls.py
+++ expenses-journal/urls.py
@@ -13,19 +13,9 @@ from django.conf.urls import include
 
 
 
-
-
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
-
 urlpatterns = [
     path("", app.views.index, name="index"),
-    path("deletenote", app.views.deletenote, name="deletenote"),
+    path(r'^deletenote/$', app.views.deletenote, name="deletenote"),
     path("db/", app.views.db, name="db"),
     path("admin/", admin.site.urls),
 	path("register/", app.views.register, name="register"),
