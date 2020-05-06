b2440f5263fa6d91fd3e4a05ad9c3949600021ee
jarekj9 <jerry.esp.sha@gmail.com>
2020-05-06 14:51:59 +0200

    test

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
 
     output=[]
     if not Categories.objects.filter(category='inne').count():
-        record = Categories(category='inne', login=User.objects.get(username='jarek').id)
+        record = Categories(category='inne', login=User.objects.get(username='jarek'))
         Categories.objects.get(category=category)
         record.save()
         
