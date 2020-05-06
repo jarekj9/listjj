37e2c95b743cb14c7997f5943ac1e1084bea15d9
jarekj9 <jerry.esp.sha@gmail.com>
2020-05-06 17:01:56 +0200

    test

--- app/views.py
+++ app/views.py
@@ -46,7 +46,7 @@ def addnote(request):
             value = form.cleaned_data['value']
             category = form.cleaned_data['category']
             description = form.cleaned_data['description']
-            note = Journal(login=request.user, date=date, value=value, category=Categories.objects.get(id=category), description=description)
+            note = Journal(login=request.user, date=date, value=value, category=category, description=description)  #Categories.objects.get(id=category)
             note.save()
             return redirect("/")
         else:
