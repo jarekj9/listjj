7cf1f6abd63ed1eecb95db49fd825d23c2b7e711
jarekj9 <jerry.esp.sha@gmail.com>
2020-05-06 14:36:33 +0200

    test

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from .models import *
-
+from django.contrib.auth.models import User
 
 def main():
 
     output=[]
     if not Categories.objects.filter(category='inne').count():
-        record = Categories(category='inne')
+        record = Categories(category='inne', login=User.objects.get(username='jarek').login_id)
+        Categories.objects.get(category=category)
         record.save()
         
     #record = Journal(login='Jarek', value=100, category=Categories.objects.get(category='inne'), description='Test')
